e57772ac812e10f88d74d1b38fa9a479a84fbeb4
MyUsername <rosanio.c@gmail.com>
2024-12-10 14:00:49 +0100

    NAVBAR - Aggiunt NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+// UTILITY
+import { NavLink } from "react-router";
+
+
 // COMPONENT EXPORT
 
 export default function Navbar() {
     return <>
-        <nav className="debug">Navbar</nav>
+        <nav className="debug">
+            <NavLink to="/" className="debug">Home</NavLink>
+            <NavLink to="/about" className="debug">About</NavLink>
+        </nav>
     </>
 }
